6cf3d9cbcc3a852eca03f29ca1cc0110538d6cae
Batoor <github@batoors.com>
2025-06-30 14:06:29 -0700

    Added Cosmos DB integration

--- src/main.py
+++ src/main.py
@@ -16,31 +16,9 @@ logging.basicConfig(
 )
 logging.getLogger("azure").setLevel(logging.WARNING)
 
-# Keys
-load_dotenv()
-api_key = os.getenv("NEWSAPI_KEY")
-key = os.getenv("LANGUAGE_KEY")
-endpoint = os.getenv("LANGUAGE_ENDPOINT")
-connection_string = os.getenv("COSMOS_CONNECTION_STRING")
 
-if not [x for x in (api_key, key, endpoint, connection_string) if x is None]:
-    pass
-else:
-    logging.error("Missing API Key")
-    sys.exit()
-
-# NewsAPI Init
-newsapi = NewsApiClient(api_key)
-language = "en"  # Article language
-
-# Azure Cosmos DB Init
-client = CosmosClient.from_connection_string(connection_string)  # type: ignore
-database = client.get_database_client("naive-gazeta")
-container = database.get_container_client("fetched_headlines")  # Change location later
-
-
-def fetched_headlines(articles):
-    """Add timestamp to each article. Send a copy to Cosmos DB"""
+def enrich_articles(articles):
+    """Add timestamp and create id using url to each article."""
     fetched_headlines = {}
     for article in articles:
         url_id = re.sub(r"[^a-zA-Z0-9]", "", article.get("url"))
@@ -49,6 +27,7 @@ def fetched_headlines(articles):
             or article["url"] is None
             or article["url"].strip() == ""
         ):
+            logging.debug("Missing url, article skipped.")
             continue
         else:
             fetched_headlines[url_id] = {
@@ -56,18 +35,15 @@ def fetched_headlines(articles):
                 "id": url_id,
                 "fetched_datetime": datetime.now().isoformat(),
             }
-            container.upsert_item(
-                fetched_headlines[url_id]
-            )  # Piggybacking off the function. Make it separate if needed.
     return fetched_headlines
 
 
 def azure_transformation(headlines):
     """Transform articles into Azure-compatible document format."""
     documents = []
-    for id, article in headlines.items():
+    for article_id, article in headlines.items():
         document = {
-            "id": article.get("url"),
+            "id": article.get("id"),
             "language": article.get("language"),
             "text": "Description: "
             + str(article.get("description"))
@@ -78,7 +54,37 @@ def azure_transformation(headlines):
     return documents
 
 
+# For now individual upsert is good enough. I'm not sending thousands of articles per second. What am I, Marcc the Zucc?
+def save_fetched_articles_to_cosmos(container, fetched_articles):
+    """Save or update fetched articles (after adding id and fetched_date) to Cosmos NoSQL DB"""
+    for article_id in fetched_articles:
+        container.upsert_item(fetched_articles[article_id])
+
+
 def main():
+    # Keys
+    load_dotenv()
+    api_key = os.getenv("NEWSAPI_KEY")
+    key = os.getenv("LANGUAGE_KEY")
+    endpoint = os.getenv("LANGUAGE_ENDPOINT")
+    connection_string = os.getenv("COSMOS_CONNECTION_STRING")
+    db_name = os.getenv("COSMOS_DB_NAME")
+    fetched_headlines = os.getenv("FETCHED_ARTICLE_CONTAINER_NAME")
+
+    # Check for missing API Keys
+    if not all([api_key, key, endpoint, connection_string, db_name, fetched_headlines]):
+        logging.error("Missing API Key")
+        sys.exit()
+
+    # NewsAPI Init
+    newsapi = NewsApiClient(api_key)
+    language = "en"  # Article language
+
+    # Azure Cosmos DB Init
+    client = CosmosClient.from_connection_string(connection_string)  # type: ignore
+    database = client.get_database_client(db_name)  # type: ignore
+    fetched_article_container = database.get_container_client(fetched_headlines)  # type: ignore
+
     # Fetch latest news headlines from NewsAPI
     try:
         top_headlines = newsapi.get_top_headlines(
@@ -103,7 +109,7 @@ def main():
             logging.info(
                 f"Successfully fetched {top_headlines.get('totalResults')} headlines."
             )
-            headlines = fetched_headlines(top_headlines["articles"])
+            headlines = enrich_articles(top_headlines["articles"])
             documents = azure_transformation(headlines=headlines)
     except Exception as e:
         logging.error(f"Document creation failed: {e}")
@@ -113,6 +119,11 @@ def main():
     if len(documents) == 0:
         logging.warning("Document creation returned empty, aborting process.")
         sys.exit()
+
+    try:
+        save_fetched_articles_to_cosmos(fetched_article_container, headlines)
+    except Exception as e:
+        logging.error(f"Unable to save fetched articles to DB: {e}")
 
     # Generate summaries using Azure Text Analytics
     try:
@@ -124,15 +135,15 @@ def main():
         sys.exit()
 
     # Display the generated summaries
+    # TODO: Ship this to DB instead
     for result in abstract_summary_results:
         if result.kind == "AbstractiveSummarization":
             logging.info("Abstracted Summary:")
-            [print(f"{summary.text}\n") for summary in result.summaries]
+            for summary in result.summaries:
+                print(f"{summary.text}\n")
         elif result.is_error is True:
             logging.error(
-                "...Is an error with code '{}' and message '{}'".format(
-                    result.error.code, result.error.message
-                )
+                f"...Is an error with code {result.error.code} and message {result.error.message}"
             )
 
 
